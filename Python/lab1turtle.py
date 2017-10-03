9eef33dabed41374396b713ceeb4029a0504d631
tntterri615 <tntterri615@gmail.com>
2017-10-03 12:34:42 -0700

    make friend for stick figure

--- Python/lab1turtle.py
+++ Python/lab1turtle.py
@@ -41,5 +41,50 @@ forward(55)
 left(50)
 forward(55)
 
+#make friend for stick figure
+
+penup()
+forward(200)
+right(120)
+forward(210)
+right(85)
+pendown()
+
+#make arms
+
+forward(100)
+left(180)
+forward(50)
+right(90)
+forward(20)
+left(90)
+
+fillcolor('green')
+begin_fill()
+
+#make head
+
+edge_length = 50
+n_sides = 25
+
+i = 0
+while i < n_sides:
+	forward(edge_length * 2/n_sides)
+	right(360/n_sides)
+	i = i + 1
+
+end_fill()
+
+#make body and legs
+
+left(90)
+forward(120)
+left(65)
+forward(55)
+left(180)
+forward(55)
+left(50)
+forward(55)
+
 done()
 
