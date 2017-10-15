da7f6a6a5370f00671366c84fb767a5b47351b78
tntterri615 <tntterri615@gmail.com>
2017-10-15 15:21:55 -0700

    initial

---
+++ Python/lab23blackjack1.py
@@ -0,0 +1,34 @@
+# give basic blackjack advise
+
+
+def card_value(card):
+    if card == 'jack' or card == 'queen' or card == 'king':
+        return 10
+    elif card == 'ace':
+            return 1
+    elif card == first_card or card == second_card or card == third_card:
+        return int(card)
+
+
+first_card = input('What is your first card? ')
+second_card = input('What is your second card? ')
+third_card = input('What is your third card? ')
+
+
+first_value = card_value(first_card)
+second_value = card_value(second_card)
+third_value = card_value(third_card)
+
+card_total = first_value + second_value + third_value
+
+print(card_total)
+
+if card_total < 17:
+    print('Hit')
+elif card_total >= 17:
+    print('Stay')
+elif card_total > 21:
+    print('Already busted')
+
+
+
