1ce26deb8b80caf9d0cb9280d04762a3740d82de
tntterri615 <tntterri615@gmail.com>
2017-10-27 16:29:51 -0700

    made recursion branch like tree

--- Python/lab29roadtrip.py
+++ Python/lab29roadtrip.py
@@ -16,10 +16,11 @@ def get_cities(city_to_visit, hops, tabs=0):
     print("--"*tabs, f"From {city_to_visit} you can visit:")
     for city in city_to_accessible_cities[city_to_visit]:
         print("--"*(tabs), "", city)
-    if hops != 1:                                                # ends recursive loop
-        for city in city_to_accessible_cities[city_to_visit]:
-            get_cities(city, hops - 1, tabs+1)
+        if hops != 1:                                                # ends recursive loop
+        # for city in city_to_accessible_cities[city_to_visit]:
+            get_cities(city, hops - 1, tabs + 1)
 
 
 start = input('Enter a city to visit (Boston, New York, Albany, Portland, or Philadelphia): ')
-get_cities(start, 3)
+number_of_hops = int(input('Enter the number of hops you want to take: '))
+get_cities(start, number_of_hops)
